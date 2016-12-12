8981a388a0ae80c3c3d5e8b1f530c8d409111ba3
Michael Akopyants <michael.akopyants@gmail.com>
2016-12-12 21:17:42 +0300

    add gitignore

---
+++ images/Classes/Controllers/Camera/CameraViewController.swift
@@ -0,0 +1,9 @@
+//
+//  CameraViewController.swift
+//  images
+//
+//  Created by Michael Akopyants on 11/12/2016.
+//  Copyright © 2016 devthanatos. All rights reserved.
+//
+
+import Foundation
---
+++ images/Classes/View/TextView/TextView.swift
@@ -0,0 +1,9 @@
+//
+//  TextView.swift
+//  images
+//
+//  Created by Michael Akopyants on 12/12/2016.
+//  Copyright © 2016 devthanatos. All rights reserved.
+//
+
+import Foundation
